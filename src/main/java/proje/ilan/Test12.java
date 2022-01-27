1f81089894d4ac0a1da9c1f82c619719f4c4bf17
Mehmet Demircioglu <demircioglumhmt@gmail.com>
2022-01-27 16:17:34 +0300

    sql delete eklendi

---
+++ src/main/java/proje/ilan/Test12.java
@@ -0,0 +1,28 @@
+package proje.ilan;
+
+public class Test12 {
+    /*
+
+    Java Temel Yazılım
+
+    -Kurulumlardan
+    JDK JRE JVM
+    -Yazılım Neden yapıyoruz?
+    -Data
+    -Java DataTypes
+    -Object Data Types => String....
+    -Variable tanımı
+    -if else if
+    -for while do while
+    -Arrays diziler
+    -method overload
+    -class
+    -interface,abstract class
+    -kalıtım,polifmorfizm
+    -Collection Framework list arraylist hashmap
+    -DB Connection
+    -Generics Java
+
+
+     */
+}
